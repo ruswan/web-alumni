c61f8c3caa8f428c4923c2b3eeac67e0cd2c6ad7
Ruswan <sukabumikode@gmail.com>
2023-08-26 11:52:03 +0700

    Create import users

--- app/Filament/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -2,9 +2,11 @@
 
 namespace App\Filament\Resources\UserResource\Pages;
 
-use App\Filament\Resources\UserResource;
 use Filament\Actions;
+use App\Filament\Resources\UserResource;
 use Filament\Resources\Pages\ListRecords;
+use Konnco\FilamentImport\Actions\ImportField;
+use Konnco\FilamentImport\Actions\ImportAction;
 
 class ListUsers extends ListRecords
 {
@@ -13,6 +15,22 @@ class ListUsers extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
+            ImportAction::make()
+            ->fields([
+                ImportField::make('name')
+                    ->label('Name')
+                    ->required()
+                    ->helperText('Nama lengkap alumni'),
+                ImportField::make('email'),
+                ImportField::make('phone_number'),
+                ImportField::make('entry_year'),
+                ImportField::make('graduation_id')
+                    ->rules('nullable|numeric'),
+                ImportField::make('major_id')
+                    ->rules('nullable|numeric'),
+                ImportField::make('consulate_id')
+                    ->rules('nullable|numeric'),
+            ]),
             Actions\CreateAction::make(),
         ];
     }
